0c25a41142d685238e87edc9d37d4ecd9a5cc8fc
royb <d}2X[1Cu5x]Sl&L93r\Z>
2019-04-30 14:53:23 +0300

    config-server init

---
+++ spring-configserver/src/main/java/com/example/configserver/springconfigserver/SpringConfigserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.configserver.springconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConfigserverApplication.class, args);
+	}
+
+}
