a7c38ef7df273cd20bebb310b187b616fd8d57c0
royb <d}2X[1Cu5x]Sl&L93r\Z>
2019-05-07 14:18:23 +0300

    Init API Gateway

---
+++ spring-apigw/src/main/java/com/example/apigw/springapigw/SpringApigwApplication.java
@@ -0,0 +1,17 @@
+package com.example.apigw.springapigw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableDiscoveryClient
+@EnableZuulProxy
+@SpringBootApplication
+public class SpringApigwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApigwApplication.class, args);
+	}
+
+}
