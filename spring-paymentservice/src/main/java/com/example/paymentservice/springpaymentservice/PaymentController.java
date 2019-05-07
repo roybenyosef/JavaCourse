bf4f59d01e798a132a5e086820fa7c96fadc1350
royb <d}2X[1Cu5x]Sl&L93r\Z>
2019-05-07 14:11:59 +0300

    Update payment rest to include instance id

--- spring-paymentservice/src/main/java/com/example/paymentservice/springpaymentservice/PaymentController.java
+++ spring-paymentservice/src/main/java/com/example/paymentservice/springpaymentservice/PaymentController.java
@@ -1,5 +1,6 @@
 package com.example.paymentservice.springpaymentservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,13 +9,17 @@ import java.lang.management.ManagementFactory;
 @RestController
 public class PaymentController {
 
+    @Value("${eureka.instance.instanceId}")
+    private String instanceId;
+
+
     @GetMapping("/paymentservice")
     public String paymentService() {
-        return "Payment Service";
+        return "Payment Service, instance: " + instanceId;
     }
 
     @GetMapping("/info")
     public String info() {
-        return "info of " + ManagementFactory.getRuntimeMXBean().getName();
+        return "info of " + instanceId;
     }
 }
