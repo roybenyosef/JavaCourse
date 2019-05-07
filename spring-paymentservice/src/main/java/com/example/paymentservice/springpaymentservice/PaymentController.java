3eaa53dc5763b5afc88afc3adc1c8d9fda3379c4
royb <d}2X[1Cu5x]Sl&L93r\Z>
2019-05-07 11:51:39 +0300

    update discovery service config

--- spring-paymentservice/src/main/java/com/example/paymentservice/springpaymentservice/PaymentController.java
+++ spring-paymentservice/src/main/java/com/example/paymentservice/springpaymentservice/PaymentController.java
@@ -3,11 +3,18 @@ package com.example.paymentservice.springpaymentservice;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.lang.management.ManagementFactory;
+
 @RestController
 public class PaymentController {
 
     @GetMapping("/paymentservice")
     public String paymentService() {
         return "Payment Service";
+    }
+
+    @GetMapping("/info")
+    public String info() {
+        return "info of " + ManagementFactory.getRuntimeMXBean().getName();
     }
 }
--- spring-userservice/src/main/java/com/example/userservice/springuserservice/UserController.java
+++ spring-userservice/src/main/java/com/example/userservice/springuserservice/UserController.java
@@ -1,13 +1,31 @@
 package com.example.userservice.springuserservice;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
+    @Autowired
+    private EurekaClient discoveryClient;
+
+
     @GetMapping("/userservice")
     public String userService() {
-        return "User Service";
+
+        InstanceInfo nextServerInfo = discoveryClient.getNextServerFromEureka("PAYMENT-SERVICE", false);
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            String jsonServerInfo = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(nextServerInfo);
+            return "User Service | Using payment service: " + jsonServerInfo;
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return "User servicve error";
     }
 }
